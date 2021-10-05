ee6cfbc0dd5120991dacaa1790cc62a07e49b84c
mGKScoder <80113093+mGKScoder@users.noreply.github.com>
2021-10-05 10:26:21 +0400

    Initial commit

---
+++ CopyCloneTest-3/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyCloneTest_3.ViewModels
+{
+   public class ItemsViewModel : ReactiveObject
+    {
+        public string textBoxValue;
+        private MainViewModel _mainVM;
+
+        private void OnCopy() => _mainVM.Copy();
+        private void OnClone() => _mainVM.Clone();
+
+
+        #region Commands
+
+        private ReactiveCommand<Unit, Unit> _cloneCommand;
+        public ReactiveCommand<Unit, Unit> CloneCommand => _cloneCommand ??= ReactiveCommand.Create<Unit, Unit>((unit) =>
+        {
+            return Unit.Default;
+        });
+        private ReactiveCommand<Unit, Unit> _copyCommand;
+        public ReactiveCommand<Unit, Unit> CopyCommand => _copyCommand ??= ReactiveCommand.Create<Unit, Unit>((unit) =>
+        {
+            return Unit.Default;
+        });
+        #endregion
+    }
+}
---
+++ CopyCloneTest-3/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyCloneTest_3.ViewModels
+{
+    public class MainViewModel :ReactiveObject
+    {
+        private ItemsViewModel[] _items = Array.Empty<ItemsViewModel>();
+        public ItemsViewModel[] Items
+        {
+            get => _items;
+            set => this.RaiseAndSetIfChanged(ref _items, value);
+        }
+        public MainViewModel()
+        {
+            var item = new ItemsViewModel();
+            Items = Items.Concat(new[] { item }).ToArray();
+        }
+
+        public void Copy()
+        {
+            var page = new ItemsViewModel();
+        }
+        public void Clone()
+        {
+            var page = new ItemsViewModel();
+
+        }
+
+      
+
+    }
+}
